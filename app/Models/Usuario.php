b162420d2159b062f337f9692c1f75b01ebbbe54
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-02-04 06:55:23 -0400

    :beers: modifica tudo e um pouco mais

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Usuario extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'senha',
+    ];
 }
