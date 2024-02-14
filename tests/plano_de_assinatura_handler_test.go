30782aa1a51517ec2e6c67d9986123309e7a3c4e
Guilherme Bernardo <bguilherme51@gmail.com>
2024-02-14 15:32:37 -0400

    :white_check_mark:  Testes para historico ok

---
+++ tests/plano_de_assinatura_handler_test.go
@@ -0,0 +1,11 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/TheDevOpsCorp/redirect-max/internal/server"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
