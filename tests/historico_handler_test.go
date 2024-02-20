e5d7468772fc1688bec978eb99d1f2a26290c847
Guilherme Bernardo <bguilherme51@gmail.com>
2024-02-20 18:25:42 -0400

    :wrench: midway implementation dos testes de usuario

--- tests/historico_handler_test.go
+++ tests/historico_handler_test.go
@@ -1,36 +1,50 @@
 package tests
 
-import (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+// import (
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
 
-	"github.com/TheDevOpsCorp/redirectify/internal/handlers/v1/api"
-	"github.com/TheDevOpsCorp/redirectify/internal/server"
-	"github.com/labstack/echo/v4"
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/TheDevOpsCorp/redirectify/internal/auth"
+// 	"github.com/TheDevOpsCorp/redirectify/internal/handlers/v1/api"
+// 	"github.com/labstack/echo/v4"
+// 	"github.com/stretchr/testify/assert"
+// 	"github.com/golang-jwt/jwt/v5"
+// )
 
-// TestSuiteHistoricoReadAll contains all tests for the HistoricoReadAll handler
-func TestSuiteHistoricoReadAll(t *testing.T) {
-	// Initialize the test server
-	_ = server.NewTestServer()
-	e := echo.New()
+// // TestSuiteHistoricoReadAll contains all tests for the HistoricoReadAll handler
+// func TestHistoricoUsuarioReadAll(t *testing.T) {
+// 	// Create a new Echo instance
+// 	e := echo.New()
 
-	// Define the common request setup
-	setupRequest := func(method, url string) (*httptest.ResponseRecorder, *echo.Context) {
-		req := httptest.NewRequest(method, url, nil)
-		res := httptest.NewRecorder()
-		c := e.NewContext(req, res)
-		return res, &c
-	}
+// 	// Create a valid JWT token
+// 	claims := jwt.MapClaims{
+// 		"username": "testuser",
+// 		// Add other claims as necessary
+// 	}
+// 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	// Test cases
-	t.Run("Pass 200: successful history retrieval", func(t *testing.T) {
-		res, c := setupRequest(http.MethodGet, "/api/historico")
+// 	// Sign the token
+// 	signedToken, _ := token.SignedString([]byte(auth.ChaveDeAcesso))
 
-		err := api.HistoricoReadAll(*c)
-		assert.NoError(t, err)
-		assert.Equal(t, http.StatusOK, res.Code)
-	})
-}
+// 	// Create a new request with the JWT token in the cookie
+// 	req := httptest.NewRequest(http.MethodGet, "/api/usuarios/historico", nil)
+// 	req.AddCookie(&http.Cookie{
+// 		Name:  "access-token",
+// 		Value: signedToken,
+// 	})
+
+// 	// Create a response recorder
+// 	res := httptest.NewRecorder()
+
+// 	// Create a new Echo context
+// 	c := e.NewContext(req, res)
+
+// 	// Call the handler
+// 	err := api.HistoricoUsuarioReadAll(c)
+
+// 	// Assert the expected response
+// 	assert.NoError(t, err)
+// 	assert.Equal(t, http.StatusOK, res.Code)
+// 	// Additional assertions for the response body
+// }
---
+++ tests/usuario_routes_test.go
@@ -0,0 +1,68 @@
+package tests
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/TheDevOpsCorp/redirectify/internal/handlers/v1/api"
+	"github.com/TheDevOpsCorp/redirectify/internal/server"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/TheDevOpsCorp/redirectify/internal/utils"
+
+)
+
+// TestSuiteUsuario contains all tests for the Usuario handlers
+func TestSuiteUsuario(t *testing.T) {
+	// Initialize the test server
+	_ = server.NewTestServer()
+	e := echo.New()
+
+	// Define the common request setup
+	setupRequest := func(method, url string) (*httptest.ResponseRecorder, *echo.Context) {
+		req := httptest.NewRequest(method, url, nil)
+		res := httptest.NewRecorder()
+		c := e.NewContext(req, res)
+		return res, &c
+	}
+
+
+	var usuarioReadAllMock = api.UsuarioReadAll
+
+	// Test cases
+	t.Run("Pass  200: successful user retrieval", func(t *testing.T) {
+		res, c := setupRequest(http.MethodGet, "/api/usuarios")
+
+		err := api.UsuarioReadAll(*c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, res.Code)
+
+		expectedBody := `{"message": "expected response body"}`  //FALTA ALTERAR AQUI AINDA
+		assert.Equal(t, expectedBody, res.Body.String())
+	})
+
+	t.Run("Pass  200: successful user retrieval by username", func(t *testing.T) {
+		res, c := setupRequest(http.MethodGet, "/api/usuarios/testuser")
+
+		err := api.UsuarioReadByNomeDeUsuario(*c)
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, res.Code)
+
+		// Check the response body
+		expectedBody := `{"message": "expected response body"}` //FALTA ALTERAR AQUI AINDA
+		assert.Equal(t, expectedBody, res.Body.String())
+	})
+	t.Run("Fail   500: database error", func(t *testing.T) {
+		res, c := setupRequest(http.MethodGet, "/api/usuarios")
+	
+		// Mock the error
+		usuarioReadAllMock = func(c echo.Context) error {
+			return utils.ErroBancoDados
+		}
+	
+		err := usuarioReadAllMock(*c)
+		assert.Error(t, err)
+		assert.Equal(t, http.StatusInternalServerError, res.Code)
+	})
+}
