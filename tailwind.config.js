6a9092302b092a8bf8aefa11e1a523ae13f26732
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-03-19 20:00:25 -0400

    :lipstick: adiciona alguns componentes e views incompletos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,34 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 const plugin = require("tailwindcss/plugin")
-const Color = require("color")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.templ"],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      rwhite: "#ffffff",
-      rblack: "#141414",
-      rdarker: "#1b5b57",
-      rdark: "#257f7a",
-      rprimary: "#35b5ae",
-      rlight: "#72cbc6",
-      rlighter: "#aee1df",
-      rlightest: "#ebf8f7",
-      rgray: "#cccccc",
-      rgraylightest: "#f2f2f2",
-      rgraylighter: "#e1e1e1",
-      rgraylight: "#dddddd",
-      rgraydark: "#bababa",
-      rgraydarker: "#7e7e7e",
-      rgraydarkest: "#434343",
-      rred: "#ff0000",
-      rredlight: "#ff9494",
-      rgreen: "#06ff00",
-      rgreenlight: "#9ad4a9",
-      rgreendark: "#34a853",
-      ryellow: "#fabb05",
-    },
     extend: {
+      colors: {
+        rwhite: "#ffffff",
+        rblack: "#141414",
+        rdarker: "#1b5b57",
+        rdark: "#257f7a",
+        rprimary: "#35b5ae",
+        rlight: "#72cbc6",
+        rlighter: "#aee1df",
+        rlightest: "#ebf8f7",
+        rgray: "#cccccc",
+        rgraylightest: "#f1f5f9",
+        rgraylighter: "#e1e1e1",
+        rgraylight: "#dddddd",
+        rgraydark: "#bababa",
+        rgraydarker: "#7e7e7e",
+        rgraydarkest: "#434343",
+        rred: "#ef4444",
+        rredlight: "#ff9494",
+        rgreen: "#06ff00",
+        rgreenlight: "#9ad4a9",
+        rgreendark: "#34a853",
+        ryellow: "#fabb05",
+      },
       maxHeight: {
         '0': '0',
         xl: '36rem',
@@ -58,25 +56,10 @@ module.exports = {
     boxShadow: ['focus'],
   },
   plugins: [
-    plugin(({ addUtilities, e, theme, variants }) => {
-      const newUtilities = {}
-      Object.entries(theme('colors')).map(([name, value]) => {
-        if (name === 'transparent' || name === 'current') return
-        const color = value[300] ? value[300] : value
-        const hsla = Color(color).alpha(0.45).hsl().string()
-
-        newUtilities[`.shadow-outline-${name}`] = {
-          'box-shadow': `0 0 0 3px ${hsla}`,
-        }
-      })
-
-      addUtilities(newUtilities, variants('boxShadow'))
-    }),
     plugin(function ({ addUtilities }) {
       addUtilities({
         ".text-last-center": {
           "textAlignLast": "center",
-
         },
       })
     })
