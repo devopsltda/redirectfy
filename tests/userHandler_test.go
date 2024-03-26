d022016c6736f8cf893df73c7ca28ca412daac6b
GuilhermeBn198 <bguilherme51@gmail.com>
2024-03-26 16:02:55 -0400

    alteração simples na estrutura do projeto e rascunho dos testes

---
+++ tests/userHandler_test.go
@@ -0,0 +1,27 @@
+package tests
+
+import (
+	"redirectify/internal/models"
+	"redirectify/internal/services/database"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUsuarios(t *testing.T) {
+
+}
+
+func TestGetUsuariosHistorico(t *testing.T) {
+
+}
+
+func TestGetUsuario(t *testing.T) {
+
+}
+
+func TestPostUsuario(t *testing.T) {
+
+}
+
+func TestPost 
