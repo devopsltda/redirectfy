5d7b8e1c7c4d724be70bcb358e1b0f7f92efe236
Eduardo Henrique <edu.hen.fm@gmail.com>
2024-03-26 16:15:53 -0400

    :test_tube: adiciona um teste de criação de usuário

--- tests/database_test.go
+++ tests/database_test.go
@@ -12,6 +12,21 @@ func TestDatabase(t *testing.T) {
 	database.New()
 	defer database.Db.Close()
 
+	t.Run("Adicionar usuário ao banco de dados", func(t *testing.T) {
+		_, err := models.UsuarioCreate(
+			database.Db,
+			"02958985261",
+			"Eduardo Henrique Freire Machado",
+			"edu_hen_fm",
+			"edu.hen.fm@gmail.com",
+			"senha-muito-complexa-aqui",
+			"2001-08-30",
+			1,
+		)
+
+		assert.NoError(t, err)
+	})
+
 	t.Run("Consultar usuários do banco de dados", func(t *testing.T) {
 		_, err := models.UsuarioReadAll(database.Db)
 
