f20e3d6f769fb173ab49012ee6168d11381ce1fc
Pablo Eduardo <heyitsmepablo@github.com>
2024-03-28 03:02:42 -0300

    :sparkles: Shared Icon Mail

---
+++ src/app/shared/icon-mail/icon-mail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-mail',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-mail.component.html',
+  styleUrl: './icon-mail.component.scss'
+})
+export class IconMailComponent {
+
+}
