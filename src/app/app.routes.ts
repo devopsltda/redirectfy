ade3d701cb09bb9ac0d87ad5b164f87909b4036f
Pablo Eduardo <heyitsmepablo@github.com>
2024-03-28 02:57:13 -0300

    :wrench:  Ajeitando o projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SignupCompleteComponent } from './pages/signup-complete/signup-complete.component';
+import { RecoveryPasswordComponent } from './pages/recovery-password/recovery-password.component';
+import { RecoverySendComponent } from './pages/recovery-send/recovery-send.component';
+import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
+import { ResetCompleteComponent } from './pages/reset-complete/reset-complete.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'login'},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'signup/complete',component:SignupCompleteComponent},
+  {path:'recovery',component:RecoveryPasswordComponent},
+  {path:'recoverySend',component:RecoverySendComponent},
+  {path:'newPassword',component:ResetPasswordComponent},
+  {path:'newPasswordComplete',component:ResetCompleteComponent}
+
+];
