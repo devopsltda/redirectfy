f32725b819539874e2f279705df02ac34c9ecca8
Pablo Eduardo <heyitsmepablo@github.com>
2024-03-31 23:32:10 -0300

    :sparkles: Add Content Grid Component

---
+++ src/app/shared/content-grid/content-grid.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-grid',
+  standalone: true,
+  imports: [],
+  templateUrl: './content-grid.component.html',
+  styleUrl: './content-grid.component.scss'
+})
+export class ContentGridComponent {
+
+}
