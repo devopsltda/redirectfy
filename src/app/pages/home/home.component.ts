58035e25867e2e2229eaca6d5c39939a88e88e98
Pablo Eduardo <heyitsmepablo@github.com>
2024-04-17 02:47:04 -0300

    :sparkles: Home Integrada a API

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,37 +4,52 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
 import { fadeInOutAnimation } from '../../animations/animations.module';
 import { RedirectifyApiService } from '../../services/redirectify-api.service';
+import { PaginatorModule, PaginatorState } from 'primeng/paginator';
 
-
+interface PageEvent {
+  first: number;
+  rows: number;
+  page: number;
+  pageCount: number;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [SharedModule,RouterModule],
+  imports: [SharedModule,RouterModule,PaginatorModule],
   animations:[fadeInOutAnimation],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  first: number  = 0;
+  rows: number = 10;
 
   homeData!:any
 
   constructor
   (
     private api:RedirectifyApiService,
-
   )
   {
 
   }
 
+
+
+  onPageChange(event: PaginatorState) {
+    this.first = event.first!=undefined?event.first:0
+    this.rows = event.rows!=undefined?event.rows:0
+}
+
  async ngOnInit() {
     this.homeData = await this.api.getAllRedirects()
-    console.log(this.homeData)
-    console.log(await this.api.createRedirect('teste','whatsapp,telegram',[{link:'teste',nome:"teste",plataforma:'teste'}]))
   }
 
-  async getHomeData(){
+  getDisplayData(){
+    const start = this.first;
+    const end = start + this.rows;
+    return this.homeData.slice(start, end);
   }
 
 }
