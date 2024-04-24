ad752194de305aeaaad3a691332e7d4443617e30
heyitsmepablo <heyitsmepablo@github.com>
2024-04-24 19:53:57 -0300

    fix(client): Roteando botões e Corrigindo responsividade

--- frontend/src/app/pages/buy-now/buy-now.component.ts
+++ frontend/src/app/pages/buy-now/buy-now.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { RedirectifyApiService } from '../../services/redirectify-api.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-buy-now',
   standalone: true,
-  imports: [SharedModule],
+  imports: [SharedModule,RouterLink],
   templateUrl: './buy-now.component.html',
   styleUrl: './buy-now.component.scss'
 })
