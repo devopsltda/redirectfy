a00feeaf570ce635b6d55f1b43e1c58210b15006
heyitsmepablo <pabloeduardossantos@gmail.com>
2024-05-02 17:58:12 -0300

    fix(client): Corrigindo comportamento do redirecionador

--- frontend/src/app/pages/redirecionador/redirecionador.component.ts
+++ frontend/src/app/pages/redirecionador/redirecionador.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RedirectifyApiService } from '../../services/redirectify-api.service';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 import { ConfirmationService } from 'primeng/api';
 import { SharedModule } from '../../shared/shared.module';
 import { RedirecionadorAnimation} from '../../animations/animations.module';
 import { IconWhatsappComponent } from '../../shared/icon-whatsapp/icon-whatsapp.component';
 import { IconTelegramComponent } from '../../shared/icon-telegram/icon-telegram.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-redirecionador',
   standalone: true,
-  imports: [ IconWhatsappComponent,IconTelegramComponent,SharedModule,ConfirmDialogModule],
+  imports: [ IconWhatsappComponent,IconTelegramComponent,SharedModule,ConfirmDialogModule,CommonModule],
   animations:[RedirecionadorAnimation],
   providers:[ConfirmationService],
   templateUrl: './redirecionador.component.html',
@@ -22,28 +24,32 @@ export class RedirecionadorComponent implements OnInit {
 
   redirectHash:string = this.activatedRoute.snapshot.params['hash']
   data:any
-  link1!:string
-  trustedLink!:SafeUrl
-  link2!:string
+
   isLoading:boolean = true
-  linkTelegram = "tg://join?invite=mjJCtz57OtU5MzBh";
-  linkWhatsapp = "whatsapp://send/?phone=5598991085854";
+  linkTelegram!:string
+  linkWhatsapp!:string
+  IsAccepted:boolean = true
+  isVisible:boolean = true
+
   constructor
   (
     private activatedRoute:ActivatedRoute,
     private api:RedirectifyApiService,
-    private sanitazer:DomSanitizer,
     private confirmationService:ConfirmationService,
   ){
-
   }
 
   async ngOnInit() {
-    this.data = await this.api.getLinksRedirect(this.redirectHash)
-    setTimeout(()=>{
-      this.openDialog()
-    },3000)
-
+    this.data = await this.api.getToLinksRedirect(this.redirectHash)
+    console.log(this.data)
+    if(this.data.body.links?.[0]?.plataforma == 'whatsapp'){
+      this.linkWhatsapp = this.data.body.links?.[0].link
+      this.linkTelegram = this.data.body.links?.[1].link
+    } else {
+      this.linkWhatsapp = this.data.body.links?.[1].link
+      this.linkTelegram = this.data.body.links?.[0].link
+    }
+    this.openDialog()
   }
 
   goLinkTelegram(){
@@ -54,19 +60,62 @@ export class RedirecionadorComponent implements OnInit {
     return  window.location.href = this.linkWhatsapp;
   }
 
-  openDialog(){
-    const linkTelegram = "tg://join?invite=+a1PN4mwBrTFkY2Ex";
-    const linkWhatsapp = "whatsapp://send/?phone=5598991085854";
-    this.confirmationService.confirm({
-      message: 'Abrir whatsapp e iniciar a conversa?',
-      accept: () => {
-        window.location.href = linkWhatsapp;
-        this.isLoading = false
-      },
-      reject: () => {
-        window.location.href = linkTelegram;
-        this.isLoading = false
-      }
-    })
+ whatsappLinkToHook(link: string): string {
+    // Expressão regular para encontrar tudo após a última '/'
+    const regex = /[^/]*$/;
+    // Encontra o que está após a última '/' no link
+    const match = link.match(regex);
+    if (match) {
+        // Pega a parte correspondente ao que está após a última '/' e adiciona ao link 'whatsapp://'
+        return 'whatsapp://' + match[0];
+    } else {
+        // Se não encontrar nada após a última '/', retorna o link original
+        return link;
+    }
+}
+telegramLinkToHook(link: string): string {
+  // Expressão regular para encontrar tudo após a última barra do "https://t.me/"
+  const regex = /https:\/\/t\.me\/([^/]+)$/;
+  // Encontra o que está após a última barra do "https://t.me/" no link
+  const match = link.match(regex);
+  if (match) {
+      // Pega a parte correspondente ao que está após a última barra do "https://t.me/" e retorna
+      return `tg://join?invite=${match[1]}`;
+  } else {
+      // Se não encontrar "https://t.me/" no link, retorna o link original
+      return link;
   }
 }
+
+   openDialog(){
+      if(this.data?.body.redirecionador.ordem_de_redirecionamento == 'whatsapp,telegram'){
+        this.confirmationService.confirm({
+          header:'Redirecionando para Whatsapp',
+          message: `Abrir whatsapp e iniciar a conversa com ${this.data.body?.redirecionador.nome} ?`,
+
+          accept: () => {
+            window.location.href = this.whatsappLinkToHook(this.linkWhatsapp)
+            this.isLoading = false
+          },
+          reject: () => {
+            this.isLoading = false
+            window.location.href = this.telegramLinkToHook(this.linkTelegram)
+          }
+        })
+      } else {
+        this.confirmationService.confirm({
+          header:'Redirecionando para Telegram',
+          message: `Abrir telegram e iniciar a conversa com ${this.data.body?.redirecionador.nome}?`,
+          accept: () => {
+            this.isLoading = false
+            window.location.href = this.telegramLinkToHook(this.linkTelegram)
+          },
+          reject: () => {
+            this.isLoading = false
+            window.location.href = this.whatsappLinkToHook(this.linkWhatsapp)
+          }
+        })
+      }
+    }
+  }
+
