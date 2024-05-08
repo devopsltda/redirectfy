854aa9ae356237351fb1451c0935047a8e7d7bf5
heyitsmepablo <pabloeduardossantos@gmail.com>
2024-05-08 15:10:34 -0300

    feat(client):Implementado o Auth nas Rotas

--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -5,6 +5,11 @@ import { CookieService } from 'ngx-cookie-service';
 
 
 export const authGuard:CanActivateFn =  (route, state) => {
-  const isAuthenticated = inject(CookieService).check('access-token') ;
+  const isAuthenticated =  inject(CookieService).check('access-token') ;
   return isAuthenticated ? true : inject(Router).navigate(['/login']);
+};
+
+export const loginGuard:CanActivateFn =  (route, state) => {
+  const isAuthenticated =  inject(CookieService).check('access-token') ;
+  return isAuthenticated ?  inject(Router).navigate(['/home']) : true;
 };
