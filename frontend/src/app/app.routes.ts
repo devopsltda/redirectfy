7a4003094d31a5d05cc91be036fa126eb3f9c44d
heyitsmepablo <pabloeduardossantos@gmail.com>
2024-05-08 15:06:46 -0300

    feat(client): Adicionado rota para redefinição de senha  Duvida na Redefinição de Senha  #18

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CreateRedirectComponent } from './pages/create-redirect/create-redirect.component';
 import { Error404Component } from './pages/404/404.component';
 import { MyAccountComponent } from './pages/my-account/my-account.component';
-import { authGuard } from './guard/auth.guard';
+import { authGuard, loginGuard } from './guard/auth.guard';
 import { BuyNowComponent } from './pages/buy-now/buy-now.component';
 import { EditRedirectComponent } from './pages/edit-redirect/edit-redirect.component';
 import { SupportComponent } from './pages/support/support.component';
@@ -20,20 +20,20 @@ import { ChangePlanComponent } from './pages/change-plan/change-plan.component';
 
 export const routes: Routes = [
   {path:'',pathMatch:'full',redirectTo:'login'},
-  {path:'login',component:LoginComponent},
+  {path:'login',component:LoginComponent, canActivate:[loginGuard]},
   {path:'finishSignup/:hashSignup',component:SignupComponent},
   {path:'signup/complete',component:SignupCompleteComponent},
   {path:'recovery',component:RecoveryPasswordComponent},
   {path:'recoverySend',component:RecoverySendComponent},
-  {path:'newPassword',component:ResetPasswordComponent},
+  {path:'newPassword/:hash',component:ResetPasswordComponent},
   {path:'newPasswordComplete',component:ResetCompleteComponent},
-  {path:'account',component:MyAccountComponent},
-  {path:'account/changePlan',component:ChangePlanComponent},
-  {path:'support',component:SupportComponent},
-  {path:'newRedirect',component:CreateRedirectComponent},
+  {path:'account',component:MyAccountComponent,canActivate:[authGuard]},
+  {path:'account/changePlan',component:ChangePlanComponent,canActivate:[authGuard]},
+  {path:'support',component:SupportComponent,canActivate:[authGuard]},
+  {path:'newRedirect',component:CreateRedirectComponent,canActivate:[authGuard]},
   {path:'buyNow',component:BuyNowComponent},
   {path:'home',component:HomeComponent,canActivate:[authGuard]},
-  {path:'home/:hash_redirect',component:EditRedirectComponent},
+  {path:'home/:hash_redirect',component:EditRedirectComponent,canActivate:[authGuard]},
   {path:'to/:hash', component:RedirecionadorComponent},
 
 
